99f52c6291c8eae4a46043d5321fa3612fa69194
DESKTOP-SPPT1MQ\Admin <290704926@qq.com>
2025-01-18 00:54:41 +0800

    3333

--- Assets/HotUpdate/Hello.cs
+++ Assets/HotUpdate/Hello.cs
@@ -5,7 +5,8 @@ public class Hello
 {
     public static void Run()
     {
-        Debug.Log("Hello, HybridCLR11122233334444rrr");
+        Debug.Log("Hello, HybridCLR11122233334444rrrxxxxx");
         Debug.Log("Hello, rrrrrrrrrrrr");
+        Debug.Log("Hello, xxxxxxxxx");
     }
 }
